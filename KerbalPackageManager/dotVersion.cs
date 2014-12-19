3db9df8c07ff7d597baadf435930b52dd6d7847f
Martin Sundhaug <martinsundhaug@gmail.com>
2014-12-20 00:54:13 +0100

    .version support

---
+++ KerbalPackageManager/dotVersion.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace KerbalPackageManager
+{
+    internal class dotVersion
+    {
+        public dotVersion(string toFilename)
+        {
+            var jObj = JObject.Parse(File.ReadAllText(toFilename));
+            Name = (string)jObj["NAME"];
+            Url = (Uri)jObj["URL"];
+            var ver = jObj["VERSION"];
+            Version = new Version((int)ver["MAJOR"], (int)ver["MINOR"], (int)ver["PATCH"], (int)ver["BUILD"]);
+        }
+
+        public string Name { get; set; }
+
+        public Uri Url { get; set; }
+
+        public Version Version { get; set; }
+    }
+}
---
+++ KerbalPackageManager/dotVersionVersion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KerbalPackageManager
+{
+    internal class dotVersionVersion
+    {
+        public int MAJOR, MINOR, PATCH, BUILD;
+    }
+}
